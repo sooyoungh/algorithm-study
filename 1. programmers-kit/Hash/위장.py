03d322234716fe4d2b7397859468f090844938b3
Sooyoung <sooyoung.h8@gmail.com>
2022-01-29 19:43:56 +0900

    Create 위장.py

---
+++ 1. programmers-kit/Hash/위장.py
@@ -0,0 +1,18 @@
+def solution(clothes):
+    # 항목별 개수를 딕셔너리로
+    data = dict()
+    
+    for i in range(len(clothes)):
+        name = clothes[i][1]
+        data[name] = data.get(name, 0) + 1
+
+    if len(data.keys()) == 1:
+        answer = len(clothes)
+    else:
+        answer = 1
+        for k,v in data.items():
+            answer *= (v+1) # 해당을 의상을 안입은 경우도 포함
+            
+        answer-= 1
+        
+    return answer
