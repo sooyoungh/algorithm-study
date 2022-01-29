69c3d5d6662bf1a1965dacd696eafdf65e7a6f79
Sooyoung <sooyoung.h8@gmail.com>
2022-01-29 19:45:07 +0900

    Update 위장.py

--- 1. programmers-kit/Hash/위장.py
+++ 1. programmers-kit/Hash/위장.py
@@ -6,13 +6,10 @@ def solution(clothes):
         name = clothes[i][1]
         data[name] = data.get(name, 0) + 1
 
-    if len(data.keys()) == 1:
-        answer = len(clothes)
-    else:
-        answer = 1
-        for k,v in data.items():
-            answer *= (v+1) # 해당을 의상을 안입은 경우도 포함
-            
-        answer-= 1
+    answer = 1
+    for k,v in data.items():
+        answer *= (v+1) # 해당을 의상을 안입은 경우도 포함
+
+    answer-= 1
         
     return answer
