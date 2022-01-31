c61de916f3a0433690a709f6e7571287b251be69
Sooyoung <sooyoung.h8@gmail.com>
2022-01-31 18:49:41 +0900

    Update 가장 큰 수.py

--- 1. programmers-kit/Sort/가장 큰 수.py
+++ 1. programmers-kit/Sort/가장 큰 수.py
@@ -1,3 +1,4 @@
+# 1. 4자리수 맞춰서 비교
 def solution(numbers):
     numbers = [str(x) for x in numbers]
     # 수 대소비교도 문자로 바꿔서
@@ -7,3 +8,17 @@ def solution(numbers):
     else:
         numbers = ''.join(numbers)
         return numbers
+
+# 2. 두 수 합쳤을 때 큰 거로
+import functools
+
+def comparator(a,b):
+    t1 = a+b
+    t2 = b+a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
