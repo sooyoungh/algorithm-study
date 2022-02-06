f5fdeb9ff29bcf2e6b908ed07ee1ce0e9056eeea
Sooyoung <sooyoung.h8@gmail.com>
2022-02-06 20:16:56 +0900

    Create BinarySearch.py

---
+++ 2. python-for-coding-test/part3/BinartSearch/BinarySearch.py
@@ -0,0 +1,24 @@
+
+# 이진탐색 - 재귀함수-> 끝나는 시점을 정해줘야
+def binary_search(arr, target, start, end):
+  if start > end:
+    return None
+  mid = (start + end)//2
+  if arr[mid] == target:
+    return mid
+  elif arr[mid] > target:
+    return binary_search(arr, target, start, mid - 1)
+  else:
+    return binary_search(arr, target, mid +1, end)
+
+# 이진탐색 - 반복문
+def binary_search(arr, target, start, end):
+  while start > end:
+    mid = (start + end)//2
+    if arr[mid] == target:
+      return mid
+    elif arr[mid] > target:
+      end = mid - 1
+    else:
+      start = mid + 1
+  return None
