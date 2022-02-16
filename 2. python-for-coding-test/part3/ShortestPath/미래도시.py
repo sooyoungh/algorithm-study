dde1eb1a372938307c0eb5a9e108963c6d78559c
Sooyoung <sooyoung.h8@gmail.com>
2022-02-16 17:06:37 +0900

    Update 미래도시.py

--- 2. python-for-coding-test/part3/ShortestPath/미래도시.py
+++ 2. python-for-coding-test/part3/ShortestPath/미래도시.py
@@ -48,3 +48,41 @@ if dist_first == -1 or dist_sec == -1:
 else:
   result = dist_first + dist_sec
   print(result)
+
+  
+  
+# 2. 플로이드
+  
+INF = int(1e9)
+n,m = map(int, input().split())
+# 최초는 무한으로 초기화
+graph = [[INF] * (n+1) for i in range(n+1)]
+
+# 자기자신 0으로 초기화
+for a in range(n+1):
+  for b in range(n+1):
+    if a== b:
+      graph[a][b] = 0
+
+# 거리 초기화
+for i in range(m):
+  a,b = map(int, input().split())
+  graph[a][b] = 1
+  graph[b][a] = 1
+
+# 최종 장소 x, 거쳐갈 장소 k
+x, k = map(int, input().split())
+
+# 플로이드 워셜
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    for k in range(1, n+1):
+      graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+# 정답 찾기
+dist = graph[1][k] + graph[k][x]
+
+if dist == INF:
+  print(-1)
+else:
+  print(dist)
