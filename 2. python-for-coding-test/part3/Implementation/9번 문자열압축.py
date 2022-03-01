7d620d866391dca13c78874fc02a296fa7300a5d
Sooyoung <sooyoung.h8@gmail.com>
2022-03-01 17:24:00 +0900

    Update 9번 문자열압축.py

--- 2. python-for-coding-test/part3/Implementation/9번 문자열압축.py
+++ 2. python-for-coding-test/part3/Implementation/9번 문자열압축.py
@@ -7,21 +7,33 @@ def solution(s):
         compress = ""
         cnt = 1
         prev = s[0:step]
-        # 파이썬 반복문 -> 증가폭, n만 빼고 가능한 i 돌기!
-        for j in range(step,n,step):
-            if prev == s[j:j+step]:
+        # 파이썬 반복문 -> 증가폭, 
+        # n-step만큼 전까지만 가능!
+        for k in range(step, n, step):
+            if prev == s[k:k+step]:
                 cnt += 1
             else:
                 if cnt>= 2:
                     compress += str(cnt) + prev
                 else:
                     compress += prev
-                prev = s[j:j+step]
+                prev = s[k:k+step]
+                # k부터 k+step까지 (인덱스랑 달리 슬라이싱은 끝보다 큰 값이어도 가능, 그냥 마지막까지 출력됨!)
+                print(k,k+step, end= ' ')
+                print()
                 cnt = 1
+                
+        # prev는 무조건 마지막까지 남은 문자열!
+        print(prev)
+        
+        # 남은 문자열 처리
         if cnt>= 2:
             compress += str(cnt) + prev
         else:
             compress += prev
+        
         answer = min(answer, len(compress))
+        
+        
         
     return answer
