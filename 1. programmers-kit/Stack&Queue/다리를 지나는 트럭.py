27b76e249cafe0151f62cd4b4aa7f6b9d76fde07
Sooyoung <sooyoung.h8@gmail.com>
2022-03-08 10:10:02 +0900

    Update 다리를 지나는 트럭.py

--- 1. programmers-kit/Stack&Queue/다리를 지나는 트럭.py
+++ 1. programmers-kit/Stack&Queue/다리를 지나는 트럭.py
@@ -1,6 +1,8 @@
+
 # 스택을 접근하는 거랑 pop하는 거랑 다른 점을 이용
 # 주의: 큐에서 빼낼때는 len(q) > 0 이어야 함!
 
+# 1. 내풀이 - 구현처럼
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
     answer = 0
@@ -33,3 +35,23 @@ def solution(bridge_length, weight, truck_weights):
             
     
     return answer+bridge_length
+
+
+# 2. sum함수가 오래 걸려서 변수로 만들고, 뺴고 더하기
+def solution(bridge_length, weight, truck_weights):
+    time = 0
+    q = [0] * bridge_length
+    sum = 0
+    while q:
+        time += 1
+        sum -= q.pop(0)
+        
+        if truck_weights:
+            if sum + truck_weights[0] <= weight:
+                cost = truck_weights.pop(0)
+                q.append(cost)
+                sum += cost
+            else:
+                q.append(0)
+    
+    return time
