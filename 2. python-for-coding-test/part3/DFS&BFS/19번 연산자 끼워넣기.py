290cdb3672cc9d79277de03fb09ba95c3e8e7f3e
Sooyoung <sooyoung.h8@gmail.com>
2022-03-09 22:35:23 +0900

    Create 19번 연산자 끼워넣기.py

---
+++ 2. python-for-coding-test/part3/DFS&BFS/19번 연산자 끼워넣기.py
@@ -0,0 +1,34 @@
+n = int(input())
+data = list(map(int, input().split()))
+add,sub,mul,div = map(int, input().split())
+
+min_val = 1e9
+max_val = -1e9
+
+def dfs(i, now):
+  global min_val, max_val, add,sub,mul,div
+  if i == n:
+    min_val = min(min_val, now)
+    max_val = max(max_val, now)
+  else:
+    if add > 0:
+      add -= 1
+      dfs(i+1, now + data[i])
+      add += 1
+    if sub > 0:
+      sub -= 0
+      dfs(i+1, now - data[i])
+      sub += 1
+    if mul > 0:
+      mul -= 1
+      dfs(i+1, now * data[i])
+      mul += 1
+    if div > 0:
+      div -= 1
+      dfs(i+1, int(now/data[i])
+      div += 1
+          
+          
+          
+          
+(파이썬 음수 나누기).[https://yunmorning.tistory.com/35]
