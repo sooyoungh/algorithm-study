c408d020ea9a7760b607418e05069390a2393efb
Sooyoung <sooyoung.h8@gmail.com>
2022-03-15 17:01:49 +0900

    Update 섬 연결하기.py

--- 1. programmers-kit/Greedy/섬 연결하기.py
+++ 1. programmers-kit/Greedy/섬 연결하기.py
@@ -1,3 +1,4 @@
+# 1. 이코테 교재 풀이 활용
 def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
@@ -29,4 +30,19 @@ def solution(n, costs):
 
     
     print(costs)
+    return answer
+
+# 2. 크루스칼 다른 풀이 - set, update 활용!
+def solution(n, costs): 
+    answer = 0 
+    costs.sort(key = lambda x: x[2]) # 비용기준으로 오름차순 정렬 
+    connect = set([costs[0][0]]) # 연결을 확인하는 집합 Kruskal 알고리즘으로 최소 비용 구하기 
+    while len(connect) != n: 
+        for cost in costs: 
+            if cost[0] in connect and cost[1] in connect: 
+                continue 
+            if cost[0] in connect or cost[1] in connect: 
+                connect.update([cost[0], cost[1]])
+                answer += cost[2] 
+                break
     return answer
