b95277bf484f63e3165d13143baeab51c88ce45e
Sooyoung <sooyoung.h8@gmail.com>
2022-03-16 15:03:00 +0900

    Update 16번 연구소.py

--- 2. python-for-coding-test/part3/DFS&BFS/16번 연구소.py
+++ 2. python-for-coding-test/part3/DFS&BFS/16번 연구소.py
@@ -1,5 +1,60 @@
 # 벽 3개 세울때 조합 사용, 2차원 배열 복사
-# **DFS + 조합으로 풀이했으나, BFS가 시간이 빠름
+# 1. BFS + 조합 4초
+from itertools import combinations
+from collections import deque
+import copy
+ 
+N, M = map(int, input().split())
+graph = []
+blank = []
+virus = []
+ 
+for i in range(N):
+  graph.append(list(map(int, input().split())))
+  for j in range(M):
+    if graph[i][j] == 0:
+      blank.append((i, j))
+    elif graph[i][j] == 2:
+      virus.append((i, j))
+ 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+ 
+def bfs():
+  testgraph = copy.deepcopy(graph)
+  q = deque(virus)
+  while q:
+    x, y = q.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < N and 0 <= ny < M:
+          if testgraph[nx][ny] == 0:
+            testgraph[nx][ny] = 2
+            q.append((nx, ny))
+ 
+  count = 0
+  for i in range(N):
+    for j in range(M):
+      if testgraph[i][j] == 0:
+        count += 1
+  
+  return count
+ 
+ 
+answer = 0
+for data in combinations(blank, 3):
+  for x, y in data:
+    graph[x][y] = 1
+  answer = max(answer, bfs()) # 이떄, 벽만 설치하고, 내부에서 바이러스 퍼지는건 원본 복사 후 진행!
+  for x, y in data:
+    graph[x][y] = 0
+ 
+print(answer)
+
+
+
+# 2. DFS + 조합으로 풀이 6초
 import sys
 import copy
 from itertools import combinations
@@ -75,6 +130,7 @@ for case in combinations(empty, 3):
     data[x][y] = 1
   dfs_start(data)  ==> 여기서 원본 데이터도 변경(바이러스)되니까, 이 문제에서는 원본을 따로 복사하여 탐색해야함
                         반면 20번 감시 피하기 문제에서는 process()에서 원본을 바꾸지 않으니까 가능함
+                      혹은 여기서도 중간에 복사해서 원본 데이터를 바꾸지 않으면 가능!
   for x,y in case:
     data[x][y] = 0
   
@@ -82,7 +138,7 @@ for case in combinations(empty, 3):
 
 
 
-# 이코테- 시간초과 (DFS 2번 사용)
+# 이코테- 시간초과-------------------------------------------------------- (DFS 2번 사용)
 n,m = map(int, inp7ut().split())
 data = []
 temp = [[0]*m  for i in range(n)]
