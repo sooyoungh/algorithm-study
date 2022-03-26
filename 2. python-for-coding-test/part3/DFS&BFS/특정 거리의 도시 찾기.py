b7b5278deaf281cbc35214e2f1fa23ef3b89e042
Sooyoung <sooyoung.h8@gmail.com>
2022-03-26 20:36:38 +0900

    Update 특정 거리의 도시 찾기.py

--- 2. python-for-coding-test/part3/DFS&BFS/특정 거리의 도시 찾기.py
+++ 2. python-for-coding-test/part3/DFS&BFS/특정 거리의 도시 찾기.py
@@ -1,3 +1,4 @@
+# 1. BFS - 연결된 노드 통해서 최단 거리 찾기
 from collections import deque
 
 n,m,k,x = map(int, input().split())
@@ -19,6 +20,29 @@ while queue:
       distance[i] = distance[now] + 1
       queue.append(i)
 
-for i in range(1, len(distance)):
+for i in range(1, n+1):
   if distance[i] == k:
     print(i)
+
+# 2. 다익스트라
+import heapq
+n, m, k, x = map(int, input().split())
+graph = [[] for i in range(n+1)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+distance = [-1] * (n+1)
+distance[x] = 0
+
+q = [(0, x)]
+
+while q:
+    dist, node = heapq.heappop(q)
+    for next in graph[node]:
+        if distance[next] == -1:
+            heapq.heappush(q, (dist + 1, next))
+            distance[next] = dist + 1
+
+print(distance)
