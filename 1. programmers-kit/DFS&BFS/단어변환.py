cd8ec0eb35dcb18e8298e2e085249f5d2ae524ad
Sooyoung <sooyoung.h8@gmail.com>
2022-06-25 18:10:14 +0900

    Update 단어변환.py

--- 1. programmers-kit/DFS&BFS/단어변환.py
+++ 1. programmers-kit/DFS&BFS/단어변환.py
@@ -1,5 +1,6 @@
 # BFS
 # 단어 하나가 다른건 두 노드가 연결된 간선이 있는 것과 같음!
+# 1. 
 from collections import deque
 def solution(begin, target, words):
     answer = 0
@@ -27,5 +28,43 @@ def solution(begin, target, words):
     
     return answer
   
-  
+  # 2.  함수화
+from collections import deque
+def solution(begin, target, words):
+    n = len(words)
+    visited = [False] * n
+    
+    def check(origin, new):
+        n = len(origin)
+        cnt = n
+        for i in range(n):
+            if origin[i] != new[i]:
+                cnt -= 1
+
+        if cnt == n-1:
+            return True
+        else:
+            return False
+    
+    def bfs():
+        q = deque()
+        q.append([begin, 0])
+        while q:
+            now, cnt = q.popleft()
+            if now == target:
+                print(cnt)
+                break
+
+            for new in words:
+                if not visited[words.index(new)]:
+                    if check(now, new):
+                        q.append([new, cnt+1])
+
+        return cnt
+    
+    if not (target in words):
+        return 0
+    else:
+        answer = bfs()
+    return answer
   
