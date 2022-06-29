ec9405b4c7b6fcdd86f3e1557100b53f2ef3229e
Sooyoung <sooyoung.h8@gmail.com>
2022-06-29 22:35:33 +0900

    Update 2. 거리두기 확인하기.py

--- 4. kakao/2021 internship/2. 거리두기 확인하기.py
+++ 4. kakao/2021 internship/2. 거리두기 확인하기.py
@@ -48,3 +48,55 @@ def solution(places):
             answer.append(0)
     
     return answer
+
+
+# 2. 
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def bfs(array, start_x, start_ay):
+    visited = [[False]*5 for _ in range(5)]
+    flag = True
+    q = deque()
+    q.append((0, (start_x, start_ay)))
+
+    while q:
+        dist, (x, y) = q.popleft()
+        visited[x][y] = True
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < 5 and 0 <= ny < 5:
+                if not visited[nx][ny]:  # O을 또 방문할 경우 제외하기
+                    if array[nx][ny] == 'O' and dist < 2:
+                        q.append((dist+1, (nx, ny)))
+                        visited[nx][ny] = True
+                    if array[nx][ny] == 'P' and dist < 2:
+                        flag = False
+                        return flag
+    return flag
+
+
+def solution(places):
+    answer = []
+
+    for i in range(5):
+        array = places[i]
+        flag = 1
+
+        for i in range(5):
+            for j in range(5):
+                if array[i][j] == 'P':
+                    if not bfs(array, i, j):
+                        flag = 0
+                        break
+            if not flag:  # 이중 for 문이므로 여기서 break!
+                break
+
+        answer.append(flag)
+
+    return answer
+
