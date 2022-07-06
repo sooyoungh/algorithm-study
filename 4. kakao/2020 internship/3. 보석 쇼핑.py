af5b156a140dc8be1ac703457d19fcd575501bca
Sooyoung <sooyoung.h8@gmail.com>
2022-07-06 22:04:09 +0900

    Update 3. 보석 쇼핑.py

--- 4. kakao/2020 internship/3. 보석 쇼핑.py
+++ 4. kakao/2020 internship/3. 보석 쇼핑.py
@@ -21,3 +21,36 @@ def solution(gems):
     return answer
 
 # 2. 투 포인터 알고리즘
+def solution(gems):
+    n = len(gems)
+    answer = [0, n-1]
+    cate = len(set(gems))
+    start = 0
+    end = 0
+    dic = {gems[0]: 1, }  # 지금 담겨있는 딕셔너리
+
+    while start < n and end < n:
+        # 모든 종류가 아직 안담긴 경우
+        if len(dic) < cate:
+            end += 1
+            if end == n:
+                break
+            dic[gems[end]] = dic.get(gems[end], 0) +1  # 없을 경우 1
+        else:   # 모든 종류 있는 경우
+            
+            # 갱신 필요
+            length = end - start + 1
+            if length < (answer[1] - answer[0] + 1):  
+                answer = [start, end]
+            
+            if dic[gems[start]] == 1:
+                del dic[gems[start]]
+            else:
+                dic[gems[start]] -= 1
+            
+            start += 1
+
+    answer[0] += 1
+    answer[1] += 1
+
+    return answer
