5cf1c6b2f4df223be2bb2217ceb7e7ba781fc5e1
Sooyoung <sooyoung.h8@gmail.com>
2022-07-08 11:01:41 +0900

    Update 불량 사용자..py

--- 4. kakao/bit/불량 사용자..py
+++ 4. kakao/bit/불량 사용자..py
@@ -34,33 +34,41 @@ def solution(user_id, banned_id):
 # 순서 상관 있음
 # depth로 원소 갯수 제한
 
+
+# 순서 상관 있음
+# depth로 원소 갯수 제한
+
 def solution(user_id, banned_id):
     answer = []
-    backtracking(0, user_id, banned_id, 0, answer)
-    return len(answer)
-
-
-def backtracking(depth, user_ids, banned_ids, current, ans):
-    if depth == len(banned_ids):
-        if current in ans:
-            return 0
-        ans.append(current)
-        return 1
-
-    res = 0
-    for i in range(len(user_ids)):
-        if current & (1 << i) or not possible(user_ids[i], banned_ids[depth]):
-            continue
-        current |= 1 << i  # 원소 추가
-        res += backtracking(depth + 1, user_ids, banned_ids, current, ans)
-        current ^= 1 << i  # 원소 삭제
-    return res
-
-
-def possible(user_id, banned_id):
-    if len(user_id) != len(banned_id):
-        return False
-    for uid, bid in zip(user_id, banned_id):
-        if bid != '*' and uid != bid:
+    
+    # 아이디 일치 여부 확인
+    def possible(user_id, banned_id):
+        if len(user_id) != len(banned_id):
             return False
-    return True
+        for uid, bid in zip(user_id, banned_id):
+            if bid != '*' and uid != bid:
+                return False
+        return True
+
+    # 백트래킹 -> depth 체크(id 갯수 제한)
+    def backtracking(depth, current):
+        nonlocal user_id, banned_id, answer
+        if depth == len(banned_id):
+            if current in answer:
+                return 0
+            answer.append(current)
+            return 1
+
+        res = 0
+        for i in range(len(user_id)):
+            if current & (1 << i) or not possible(user_id[i], banned_id[depth]):
+                continue
+            current |= 1 << i  # 원소 추가
+            res += backtracking(depth + 1, current)
+            current ^= 1 << i  # 원소 삭제
+        return res
+
+    # 시작
+    backtracking(0, 0)
+
+    return len(answer)
