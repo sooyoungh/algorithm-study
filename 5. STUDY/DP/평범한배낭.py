bfc0cd961cf2c8fb07eca2931de8b0b0956eab74
Sooyoung <sooyoung.h8@gmail.com>
2022-09-03 11:31:50 +0900

    Update 평범한배낭.py

--- 5. STUDY/DP/평범한배낭.py
+++ 5. STUDY/DP/평범한배낭.py
@@ -1,5 +1,5 @@
 
-# 1) 1차원 DP
+# 1) 1차원 DP : top-down
 import sys
 input = sys.stdin.readline
 
@@ -15,26 +15,25 @@ for weight, value in data:
 
 print(d[-1])
 
-# 2) 2차원 DP
-# input = sys.stdin.readline
+# 2) 2차원 DP : bottom-up
+input = sys.stdin.readline
 
-# n, k = map(int, input().split())
-# data = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
-# for i in range(n):
-#     data.append(list(map(int, input().split())))
-# # [list(map(int, input().split())) for _ in range(n)]
+n, k = map(int, input().split())
+data = [[0, 0]] + [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    data.append(list(map(int, input().split())))
 
-# dp = [[0]*(k+1) for _ in range(n+1)]
+dp = [[0]*(k+1) for _ in range(n+1)]
 
-# for i in range(1, n+1):  # 물건 루프
-#     for j in range(1, k+1):  # 무게 루프
-#         # 현재 물건(i)의 무게, 가치
-#         weight = data[i][0]
-#         value = data[i][1]
+for i in range(1, n+1):  # 물건 루프
+    for j in range(1, k+1):  # 무게 루프
+        # 현재 물건(i)의 무게, 가치
+        weight = data[i][0]
+        value = data[i][1]
 
-#         if j < weight:
-#             dp[i][j] = dp[i-1][j]
-#         else:
-#             dp[i][j] = max(dp[i-1][j], value + dp[i-1][j-weight])
+        if j < weight:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], value + dp[i-1][j-weight])
 
-# print(dp[n][k])
+print(dp[n][k])
