56a90087d86238f6713e55afe9ba8e747da090a4
Sooyoung <sooyoung.h8@gmail.com>
2022-09-23 10:03:14 +0900

    Update 가장긴증가하는부분수열.py

--- 5. STUDY/DP/가장긴증가하는부분수열.py
+++ 5. STUDY/DP/가장긴증가하는부분수열.py
@@ -5,12 +5,12 @@ input = sys.stdin.readline
 n = int(input())
 data = list(map(int, input().split()))
 
-dp = [1] * (n)
+dp = [1] * (n) # 초기화
 
 for i in range(n):  # i일때
     for j in range(i):  # i전까지의 원소들을 비교했을때
-        if data[i] > data[j]:  # 이전 원소들이 더 크다면,
-            dp[i] = max(dp[i], dp[j]+1)  # max 비교해서 바꿔주기
+        if data[i] > data[j]:  # 증가수열이라면
+            dp[i] = max(dp[i], dp[j]+1)  # 짧은 증가수열값이 이미 update되어 있을 수 있으니, max 비교해서 바꿔주기
 
 print(max(dp))
 
