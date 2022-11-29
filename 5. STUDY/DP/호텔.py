6de93908f559315a85ae3e90c62bcae387a14b12
Sooyoung <sooyoung.h8@gmail.com>
2022-11-29 13:51:01 +0900

    Update 호텔.py

--- 5. STUDY/DP/호텔.py
+++ 5. STUDY/DP/호텔.py
@@ -4,18 +4,14 @@ input = sys.stdin.readline
 # Q) 최소 c명이상 일때의 최소 비용?
 c, n = map(int, input().split())
 
-# 홍보비, 얻는 손님수
 data = [list(map(int, input().split())) for _ in range(n)]
-
 data.sort()
 
-# 1000대신, c+100 이어도 괜찮음!
-dp = [1e9] * (c+100)
+dp = [1e9] * (c + 100)
 dp[0] = 0
-
-for ad_cost, ad_cus in data:
-    for cus in range(ad_cus, c+100):
-        # dp[현재 손님수] = min (dp[현재 손님수 - 홍보 손님수 ] + 홍보 비용, dp[현재 손님수] )
-        dp[cus] = min(dp[cus], ad_cost + dp[cus - ad_cus])
+for cost, cus in data:
+    for i in range(cus, c + 100):
+        # dp[현재 손님수] = min (dp[현재 손님수], dp[현재 손님수 - 홍보 손님수 ] + 홍보 비용 )
+        dp[i] = min(dp[i], dp[i-cus] + cost)
 
 print(min(dp[c:]))
