9a01543c7b7c14f5f04b21e05eeb7f99f7670a09
Sooyoung <sooyoung.h8@gmail.com>
2022-12-03 13:13:21 +0900

    Update 3. 주차 요금 계산.py

--- 4. kakao/2022 blind/3. 주차 요금 계산.py
+++ 4. kakao/2022 blind/3. 주차 요금 계산.py
@@ -57,3 +57,69 @@ def solution(fees, records):
     print(new_answer)
 
     return new_answer
+
+
+# ------------------------시간 
+from collections import defaultdict
+from collections import deque
+import math
+
+
+def solution(fees, records):
+    answer = []
+    basic_time = fees[0]
+    basic_money = fees[1]
+    plus_time = fees[2]
+    plus_money = fees[3]
+
+    data = defaultdict(list)
+    time_cnt = defaultdict(int)  # 각 차량별 시간
+    money = defaultdict(int)  # 각 차량별 돈
+
+    # 1. 차 정보 입력
+    for tmp in records:
+        time, car, in_out = tmp.split()
+        car = int(car)
+        hour, min = time.split(':')
+        time = (int(hour)*60) + int(min)
+        data[car].append((time, in_out))
+    print(data)
+
+    # 딕셔너리를 키로 정렬하기 위해 key값만 정렬
+    sorted_keys = sorted(data.keys())
+
+    # 2. 차량별 시간 계산
+    for key in sorted_keys:
+        total_time = 0
+        q = deque()
+
+        for time, in_out in data[key]:
+            if in_out == 'IN':
+                q.append(time)
+            elif in_out == 'OUT':
+                in_time = q.popleft()
+                out_time = time
+                total_time += out_time - in_time
+
+        if len(q) != 0:  # 출차 안하면, 23:59로 계산
+            print("혹시..")
+            in_time = q.popleft()
+            total_time += ((23 * 60) + 59) - in_time
+        time_cnt[key] = total_time
+
+    # 3. 차량별 돈 계산
+    for car, time in time_cnt.items():
+        if time <= basic_time:
+            money[car] = basic_money
+        else:
+            tmp_time = math.ceil((time - basic_time) / plus_time)
+            money[car] = basic_money + tmp_time * plus_money
+
+    return money
+
+
+fees = [180, 5000, 10, 600]
+records = ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT",
+           "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
+
+print(solution(fees, records))
